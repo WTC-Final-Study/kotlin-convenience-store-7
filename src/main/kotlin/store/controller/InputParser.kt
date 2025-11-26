c275afb23c2e3fe094338f1d8bcb718cca38301a
baekcii <bsy8470@naver.com>
2025-11-27 02:37:35 +0900

    refactor: 에러 메세지 상수화

--- src/main/kotlin/store/controller/InputParser.kt
+++ src/main/kotlin/store/controller/InputParser.kt
@@ -1,5 +1,6 @@
 package store.controller
 
+import store.common.ErrorType
 import store.domain.model.OrderItem
 
 object InputParser {
@@ -7,14 +8,14 @@ object InputParser {
         Regex("(^(\\[[A-Za-z가-힣0-9]+-\\d+])(,\\[[A-Za-z가-힣0-9]+-\\d+])*$)|(^[A-Za-z가-힣0-9]+-\\d+$)")
 
     fun parsePurchaseItem(input: String): List<OrderItem> {
-        require(input.isNotBlank()) { "[ERROR] 잘못된 입력입니다. 다시 입력해 주세요." }
-        require(input.replace(" ", "").matches(ORDER_REGEX)) { "[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요." }
+        require(input.isNotBlank()) { ErrorType.INVALID_PURCHASE_FORMAT }
+        require(input.replace(" ", "").matches(ORDER_REGEX)) { ErrorType.INVALID_PURCHASE_FORMAT }
 
         return input.split(",")
             .map { token ->
                 val cleaned = token.trim().removeSurrounding("[", "]")
                 val parts = cleaned.split("-").map { it.trim() }
-                val count = requireNotNull(parts[1].toIntOrNull()) { "[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요." }
+                val count = requireNotNull(parts[1].toIntOrNull()) { ErrorType.INVALID_PURCHASE_FORMAT }
                 OrderItem(parts[0], count)
             }
     }
