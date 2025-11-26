4fbf7ff7e9bdc14bac65cc6fd4d33fa5c4697fe1
baekcii <bsy8470@naver.com>
2025-11-27 01:35:00 +0900

    refactor: repository 인터페이스 삭제

--- src/main/kotlin/store/data/repository/StorageRepository.kt
+++ src/main/kotlin/store/data/repository/StorageRepository.kt
@@ -2,15 +2,13 @@ package store.data.repository
 
 import store.data.local.Storage
 import store.data.mapper.EntityMapper.toDomain
-import store.domain.model.Product
 import store.domain.model.ProductStock
-import store.domain.repository.StorageRepository
 
-class StorageRepositoryImpl(
-    private val storage: Storage
-) : StorageRepository {
+class StorageRepository(
+    private val storage: Storage = Storage
+) {
 
-    override fun getProductStocks(): List<ProductStock> {
+    fun getProductStocks(): List<ProductStock> {
         val promotions = storage.promotions.map { it.toDomain() }
         val productEntities = storage.products
 
--- src/main/kotlin/store/domain/repository/StorageRepository.kt
+++
@@ -1,9 +0,0 @@
-package store.domain.repository
-
-import store.domain.model.Product
-import store.domain.model.ProductStock
-
-interface StorageRepository {
-
-    fun getProductStocks(): List<ProductStock>
-}
