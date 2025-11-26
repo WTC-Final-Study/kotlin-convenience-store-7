fe855e2e09dc865bd39261090eb904a23bbdc82f
baekcii <bsy8470@naver.com>
2025-11-26 21:18:53 +0900

    feat(data): Storage 초기화 로직 구현 및 entity 기본값 적용

--- src/main/kotlin/store/data/local/entity/ProductEntity.kt
+++ src/main/kotlin/store/data/local/entity/ProductEntity.kt
@@ -3,7 +3,7 @@ package store.data.local.entity
 data class ProductEntity(
     val name: String,
     val price: Int,
-    var promotionQuantity: Int,
-    var generalQuantity: Int,
-    val promotion: String?
+    var promotion_quantity: Int = 0,
+    var general_quantity: Int = 0,
+    val promotion: String? = null
 )
