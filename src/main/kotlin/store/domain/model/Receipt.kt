70ccb488789c99125350c69b5ffe3658c706846c
baekcii <bsy8470@naver.com>
2025-11-29 04:28:42 +0900

    refactor: 멤버십 할인 조건 추가

--- src/main/kotlin/store/domain/model/Receipt.kt
+++ src/main/kotlin/store/domain/model/Receipt.kt
@@ -5,10 +5,13 @@ data class Receipt(
     val promotionItems: List<OrderItem>,
     val hasMembershipDiscount: Boolean
 ) {
-    val totalPrice get() = orderItems.sumOf { it.product.price * it.count }
+    val totalCount get() = orderItems.sumOf { it.count }
+    val totalPrice get() = orderItems.sumOf { it.totalPrice }
+
     val promotionDiscount get() = promotionItems.sumOf { it.product.price * it.count }
     val membershipDiscount: Int
-        get() = ((totalPrice - promotionDiscount) * 30 / 100).coerceAtMost(8000)
+        get() = if (!hasMembershipDiscount) 0
+        else ((totalPrice - promotionDiscount) * 30 / 100).coerceAtMost(8000)
 
     val finalAmount get() = totalPrice - promotionDiscount - membershipDiscount
 }
