64698728abe2c3a18a8be3c83ecf58bf60c7530b
baekcii <bsy8470@naver.com>
2025-11-29 00:09:22 +0900

    feat(controller): 상품 주문 및 프로모션 상품 추가 로직 연결

--- src/main/kotlin/store/controller/Controller.kt
+++ src/main/kotlin/store/controller/Controller.kt
@@ -2,6 +2,7 @@ package store.controller
 
 import store.common.ErrorType
 import store.domain.model.OrderItem
+import store.domain.service.PurchaseService
 import store.domain.service.StorageService
 import store.presentation.mapper.UiMapper.toUiList
 import store.view.InputView
@@ -9,19 +10,46 @@ import store.view.OutputView
 
 class Controller {
 
+    private lateinit var purchaseService: PurchaseService
     fun run() {
         val storageService = StorageService()
+        purchaseService = PurchaseService(storageService)
         val productStocks = storageService.getProductStocks()
         OutputView.displayStock(productStocks.toUiList())
         purchase()
     }
 
-    private fun purchase(): List<OrderItem> {
+    private fun purchase() {
+        OutputView.displayPurchasePrompt()
+        val orderItems = getOrderItems()
+        handleMoreBenefit(orderItems)
+    }
+
+    private fun getOrderItems(): List<OrderItem> {
         OutputView.displayPurchasePrompt()
 
         return retryUntilValid {
             val input = InputView.read()
-            InputParser.parsePurchaseItem(input)
+            val parsedInput = InputParser.parsePurchaseItem(input)
+            purchaseService.order(parsedInput)
+        }
+    }
+
+    private fun handleMoreBenefit(orderItems: List<OrderItem>) {
+        for (item in orderItems) {
+            val extraCount = purchaseService.extraCountForPromotion(item)
+            if (extraCount == 0) continue
+
+            if (!confirmExtraPromotion(item.name, extraCount)) continue
+            purchaseService.addExtraItem(item.name, extraCount)
+        }
+    }
+
+    private fun confirmExtraPromotion(name: String, extra: Int): Boolean {
+        OutputView.displayExtraPrompt(name, extra)
+        return retryUntilValid {
+            val input = InputView.read()
+            InputParser.parseYesOrNo(input)
         }
     }
 
