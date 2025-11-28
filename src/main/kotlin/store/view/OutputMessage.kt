046f80857cefce805141117fd0f5ee658a97ee16
baekcii <bsy8470@naver.com>
2025-11-29 04:30:55 +0900

    feat: 영수증 출력 로직 구현

--- src/main/kotlin/store/view/OutputMessage.kt
+++ src/main/kotlin/store/view/OutputMessage.kt
@@ -6,7 +6,21 @@ enum class OutputMessage(val text: String) {
     PURCHASE_INPUT_PROMPT("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])"),
     EXTRA_ITEMS_CONFIRM("현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)"),
     NON_PROMOTION_CONFIRM("현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)"),
-    MEMBERSHIP_DISCOUNT_CONFIRM("멤버십 할인을 받으시겠습니까? (Y/N)");
+    MEMBERSHIP_DISCOUNT_CONFIRM("멤버십 할인을 받으시겠습니까? (Y/N)"),
+
+    RECEIPT_HEADER_STORE("===========W 편의점============="),
+    RECEIPT_HEADER_FREE("===========증 정============="),
+    RECEIPT_DIVIDER("=============================="),
+
+    RECEIPT_BODY_LABEL("상품명\t수량\t금액"),
+
+    RECEIPT_ITEM_WITH_PRICE("%s\t%d\t%,d"),
+    RECEIPT_FREE_ITEM("%s\t%d"),
+
+    RECEIPT_TOTAL_PRICE("총구매액\t%d\t%,d"),
+    RECEIPT_DISCOUNT("%s\t-%,d"),
+
+    RECEIPT_FINAL_AMOUNT("내실돈\t%,d");
 
     override fun toString(): String = text
 
--- src/main/kotlin/store/view/OutputView.kt
+++ src/main/kotlin/store/view/OutputView.kt
@@ -1,5 +1,7 @@
 package store.view
 
+import store.domain.model.OrderItem
+import store.domain.model.Receipt
 import store.presentation.model.ProductUi
 
 object OutputView {
@@ -30,5 +32,34 @@ object OutputView {
 
     fun displayError(message: String) {
         println(message)
+    }
+
+    fun displayReceipt(receipt: Receipt) {
+        println(OutputMessage.RECEIPT_HEADER_STORE)
+        displayOrderItems(receipt.orderItems)
+        displayFreeItems(receipt.promotionItems)
+        displayTotal(receipt)
+    }
+
+    private fun displayOrderItems(orderItems: List<OrderItem>) {
+        println(OutputMessage.RECEIPT_BODY_LABEL)
+        orderItems.forEach { item ->
+            println(OutputMessage.RECEIPT_ITEM_WITH_PRICE.format(item.product.name, item.count, item.totalPrice))
+        }
+    }
+
+    private fun displayFreeItems(promotionItems: List<OrderItem>) {
+        println(OutputMessage.RECEIPT_HEADER_FREE)
+        promotionItems.forEach { item ->
+            println(OutputMessage.RECEIPT_FREE_ITEM.format(item.product.name, item.count))
+        }
+    }
+
+    private fun displayTotal(receipt: Receipt) {
+        println(OutputMessage.RECEIPT_DIVIDER)
+        println(OutputMessage.RECEIPT_TOTAL_PRICE.format(receipt.totalCount, receipt.totalPrice))
+        println(OutputMessage.RECEIPT_DISCOUNT.format("행사할인", receipt.promotionDiscount))
+        println(OutputMessage.RECEIPT_DISCOUNT.format("멤버십할인", receipt.membershipDiscount))
+        println(OutputMessage.RECEIPT_FINAL_AMOUNT.format(receipt.finalAmount))
     }
 }
