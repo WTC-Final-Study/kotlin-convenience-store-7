02240f53d7ffc3ec859606d6364ede0af276fefe
joon0447 <joon0447@naver.com>
2025-12-18 13:25:04 +0900

    test: 주문 메뉴, 수량 검증

--- src/test/kotlin/store/service/ProductManagerTest.kt
+++ src/test/kotlin/store/service/ProductManagerTest.kt
@@ -2,6 +2,8 @@ package store.service
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
+import store.model.Order
 
 class ProductManagerTest {
 
@@ -9,6 +11,33 @@ class ProductManagerTest {
     fun `상품 파일 로드`() {
         assertDoesNotThrow {
             val productManager = ProductManager()
+        }
+    }
+
+    @Test
+    fun `존재하지 않는 메뉴를 주문하면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            val productManager = ProductManager()
+            val orders = listOf(Order("테스트", 3))
+            productManager.checkOrder(orders)
+        }
+    }
+
+    @Test
+    fun `수량을 잘못 입력하면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            val productManager = ProductManager()
+            val orders = listOf(Order("콜라", -5))
+            productManager.checkOrder(orders)
+        }
+    }
+
+    @Test
+    fun `수량이 재고보다 많으면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            val productManager = ProductManager()
+            val orders = listOf(Order("콜라", 100))
+            productManager.checkOrder(orders)
         }
     }
 }
